0d6a5c4f00857af3353a86a6897381bb52c193f3
Robert Anton <robert.anton@infor.com>
2021-04-30 15:05:09 +0200

    cleanup

--- main.py
+++ main.py
@@ -25,15 +25,6 @@ class ProcessNode:
     def set_calling(self, calling):
         self.calling.append(calling)
 
-    # def __str__(self, level=0):
-    #     ret = "\n"*level+repr(self.key)
-    #     for node in self.calling:
-    #         ret += node.__str__(level+1)
-    #     return ret
-    #
-    # def __repr__(self):
-    #     return '<tree rep>'
-
 
 globalReportsNodes = []
 globalProcessNodes = []
@@ -46,19 +37,18 @@ def processAll(file):
     for r in globalReportsNodes:
         for n in r.nodes:
             stack = []
-            pprint_tree_v(n, r, stack)
+            populateStack(n, r, stack)
             if file in stack:
+                print(f'\n******************* \n')
                 pprint_tree(n, r, file)
 
 
-def pprint_tree_v(node, r, stack, file=None, _prefix="", _last=True):
-    # print(_prefix, "`- " if _last else "|- ", node.key, sep="", file=file)
-    # _prefix += "   " if _last else "|  "
+def populateStack(node, r, stack, file=None, _prefix="", _last=True):
     child_count = len(node.calling)
     for i, child in enumerate(node.calling):
         _last = i == (child_count - 1)
         stack.insert(0, child.key)
-        pprint_tree_v(child, r, stack, file, _prefix, _last)
+        populateStack(child, r, stack, file, _prefix, _last)
 
 
 def pprint_tree(node, r, f, file=None, _prefix="", _last=True):
@@ -92,11 +82,6 @@ def setReports():
 def setNodes():
     for r, d, fls in os.walk(
             'C:\\Farms\\DEPM-35267-WFB-Perf-Model-1204\\Projects\\AppEngine\\Sources\\Depm\\Processes\\'):
-        procDir = r.split('\\')[-2]
-        # if procDir not in ['DEPMWorkforceAddIn', 'Allocation Addon', 'B&P Add in', 'DEPMCapitalPlanning', 'Deprecated',
-        #                    'OlapAPIExtension', 'DEPMDynamicAttributesAddin', 'DEPMDateTimeAddIn',
-        #                    'DEPMGenericAllocationAddin',
-        #                    'WDDataUploadAddIn', 'DEPMWorkflowAddin', 'DEPMWorkforceDesignAddIn']:
         for f in fls:
             fExt = pathlib.Path(f).suffix
             if fExt == '.bs' and f not in globalProcessNodesStr:
@@ -124,24 +109,6 @@ def setNodes():
                                 node.calling.append(subNode)
 
 
-
-def reportCallStacksClasses():
-    for reportKey, calledProcesses in readReportRef().items():
-        curr = Report(reportKey)
-        processed = []
-        processNodes = []
-        for process in calledProcesses:
-            if process not in processed:
-                processNode = ProcessNode(process, curr)
-                curr.nodes.append(processNode)
-        for node in processNodes:
-            calls(node)
-
-
-def calls(processNode):
-    pass
-
-
 def readReportRef():
     rpath = '.\\assets\\processListSimple.txt'
     with open(rpath, 'r') as fr:
@@ -158,55 +125,5 @@ def readReportRef():
         return repsDict
 
 
-def reportCallStacks():
-    res = {}
-    for reportKey, calledProcesesses in readReportRef().items():
-        processed = []
-        res[reportKey] = []
-        for process in calledProcesesses:
-            if process not in processed:
-                processed.append(process)
-                calls = [process]
-                lookforusages(
-                    'C:\\Farms\\DEPM-35267-WFB-Perf-Model-1204\\Projects\\AppEngine\\Sources\\Depm\\Processes\\',
-                    process, calls)
-                # print(f'{reportKey};{";".join([el for el in calls])}')
-                res[reportKey].append(calls)
-
-    # with open('data.txt', 'w') as outfile:
-    #     json.dump(res, outfile)
-
-
-def lookforusages(rpath, file, callsTack):
-    for r, d, fls in os.walk(rpath):
-        procDir = r.split('\\')[-2]
-        if procDir != 'DEPMWorkforceAddIn':
-            for f in fls:
-                if f != file and f not in callsTack:
-                    fExt = pathlib.Path(f).suffix
-                    if fExt == '.bs':
-                        fPath = os.path.join(r, f)
-                        with open(fPath, 'r', encoding='UTF-8') as fr:
-                            content = fr.readlines()
-                            for ln in [li.strip('\n\r\t') for li in content]:
-                                if re.search(file.split('.')[0], ln, re.IGNORECASE) and re.search('#include', ln,
-                                                                                                  re.IGNORECASE):
-                                    callsTack.append(f)
-                                    lookforusages(rpath, f, callsTack)
-
-
-def checkProcess(process):
-    with open('data.txt') as reportsDict:
-        data = json.load(reportsDict)
-        for report, callStacks in data.items():
-            for calls in callStacks:
-                if process in calls:
-                    for call in calls:
-                        print(report + ";" + call)
-
-
 if __name__ == '__main__':
-    # reportCallStacks()
-    # checkProcess('WB_ComputeAnnualFTE.bs')
-    # reportCallStacksClasses()
     processAll('WB_ComputeAnnualFTE.bs')
