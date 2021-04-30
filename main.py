0ccf4647e9f658aec959d256a68b8aa48491715f
Robert Anton <robert.anton@infor.com>
2021-04-30 14:15:15 +0200

    tree structre

--- main.py
+++ main.py
@@ -4,8 +4,146 @@ import pathlib
 import re
 
 
+class Report:
+    def __init__(self, key):
+        self.key = key
+        self.nodes = []
+
+    def setNode(self, node):
+        self.nodes.append(node)
+
+
+class ProcessNode:
+    def __init__(self, key):
+        self.key = key
+        self.callers = []
+        self.calling = []
+
+    def setCaller(self, caller):
+        self.callers.append(caller)
+
+    def set_calling(self, calling):
+        self.calling.append(calling)
+
+    # def __str__(self, level=0):
+    #     ret = "\n"*level+repr(self.key)
+    #     for node in self.calling:
+    #         ret += node.__str__(level+1)
+    #     return ret
+    #
+    # def __repr__(self):
+    #     return '<tree rep>'
+
+
+globalReportsNodes = []
+globalProcessNodes = []
+globalProcessNodesStr = []
+
+
+def processAll(file):
+    setNodes()
+    setReports()
+    for r in globalReportsNodes:
+        for n in r.nodes:
+            stack = []
+            pprint_tree_v(n, r, stack)
+            if file in stack:
+                pprint_tree(n, r)
+
+
+def pprint_tree_v(node, r, stack, file=None, _prefix="", _last=True):
+    # print(_prefix, "`- " if _last else "|- ", node.key, sep="", file=file)
+    # _prefix += "   " if _last else "|  "
+    child_count = len(node.calling)
+    for i, child in enumerate(node.calling):
+        _last = i == (child_count - 1)
+        stack.insert(0, child.key)
+        pprint_tree_v(child, r, stack, file, _prefix, _last)
+
+
+def pprint_tree(node, r, file=None, _prefix="", _last=True):
+    print(r.key, _prefix, "`- " if _last else "|- ", node.key, sep="", file=file)
+    _prefix += "   " if _last else "|  "
+    child_count = len(node.calling)
+    for i, child in enumerate(node.calling):
+        _last = i == (child_count - 1)
+        pprint_tree(child, r, file, _prefix, _last)
+
+
+def setReports():
+    globalProcessNodes.sort(key=lambda n: n.key, reverse=False)
+    for reportKey, calledProcesses in readReportRef().items():
+        curr = Report(reportKey)
+        globalReportsNodes.append(curr)
+        processed = []
+        for process in calledProcesses:
+            if process not in processed:
+                processed.append(process)
+                procNode = None
+                for n in globalProcessNodes:
+                    if n.key.lower() == process.lower():
+                        procNode = n
+                        continue
+                if procNode is not None:
+                    curr.setNode(procNode)
+                    procNode.setCaller(curr)
+
+
+def setNodes():
+    for r, d, fls in os.walk(
+            'C:\\Farms\\DEPM-35267-WFB-Perf-Model-1204\\Projects\\AppEngine\\Sources\\Depm\\Processes\\'):
+        procDir = r.split('\\')[-2]
+        # if procDir not in ['DEPMWorkforceAddIn', 'Allocation Addon', 'B&P Add in', 'DEPMCapitalPlanning', 'Deprecated',
+        #                    'OlapAPIExtension', 'DEPMDynamicAttributesAddin', 'DEPMDateTimeAddIn',
+        #                    'DEPMGenericAllocationAddin',
+        #                    'WDDataUploadAddIn', 'DEPMWorkflowAddin', 'DEPMWorkforceDesignAddIn']:
+        for f in fls:
+            fExt = pathlib.Path(f).suffix
+            if fExt == '.bs' and f not in globalProcessNodesStr:
+                fPath = os.path.join(r, f)
+                node = ProcessNode(f)
+                if node is None:
+                    print('is None!')
+                globalProcessNodes.append(node)
+                globalProcessNodesStr.append(f)
+                with open(fPath, 'r', encoding='UTF-8') as fr:
+                    content = fr.readlines()
+                    for ln in [li.strip('\n\r\t') for li in content]:
+                        if re.search('#include', ln, re.IGNORECASE):
+                            txt = re.split('\"', ln)
+                            subNodeName = txt[1] + '.bs'
+                            subNode = None
+                            for n in globalProcessNodes:
+                                if n.key.lower() == subNodeName.lower():
+                                    subNode = n
+                                    break
+                            else:
+                                subNode = ProcessNode(subNodeName)
+                            if subNode is not None:
+                                subNode.callers.append(node)
+                                node.calling.append(subNode)
+
+
+
+def reportCallStacksClasses():
+    for reportKey, calledProcesses in readReportRef().items():
+        curr = Report(reportKey)
+        processed = []
+        processNodes = []
+        for process in calledProcesses:
+            if process not in processed:
+                processNode = ProcessNode(process, curr)
+                curr.nodes.append(processNode)
+        for node in processNodes:
+            calls(node)
+
+
+def calls(processNode):
+    pass
+
+
 def readReportRef():
-    rpath = 'C:\\Farms\\2021-03-30\\2021-03-30\\processListSimple.txt'
+    rpath = '.\\assets\\processListSimple.txt'
     with open(rpath, 'r') as fr:
         content = fr.readlines()
         curr = ''
@@ -20,37 +158,31 @@ def readReportRef():
         return repsDict
 
 
-def fromReportsToProcess(checkProcess):
-    reports = readReportRef()
-    inReports = []
-    for key, value in reports.items():
-        for v in value:
-            if v == checkProcess and key not in inReports:
-                inReports.append(key)
-    return inReports
+def reportCallStacks():
+    res = {}
+    for reportKey, calledProcesesses in readReportRef().items():
+        processed = []
+        res[reportKey] = []
+        for process in calledProcesesses:
+            if process not in processed:
+                processed.append(process)
+                calls = [process]
+                lookforusages(
+                    'C:\\Farms\\DEPM-35267-WFB-Perf-Model-1204\\Projects\\AppEngine\\Sources\\Depm\\Processes\\',
+                    process, calls)
+                # print(f'{reportKey};{";".join([el for el in calls])}')
+                res[reportKey].append(calls)
+
+    # with open('data.txt', 'w') as outfile:
+    #     json.dump(res, outfile)
 
 
-def processSearchPrint(callstack):
-    for caller in callstack:
-        idx = callstack.index(caller)
-        reports = fromReportsToProcess(caller)
-        if len(reports) > 0:
-            tmp = f'{caller} => reports;{";".join([e for e in reports])}'
-            callstack.remove(caller)
-            callstack.append(tmp)
-
-
-
-def lookforusages(file, rootpath):
-    callstack = []
-    for r, d, fls in os.walk(rootpath):
+def lookforusages(rpath, file, callsTack):
+    for r, d, fls in os.walk(rpath):
         procDir = r.split('\\')[-2]
-        if procDir not in ['DEPMWorkforceAddIn', 'Allocation Addon', 'B&P Add in', 'DEPMCapitalPlanning', 'Deprecated',
-                           'OlapAPIExtension', 'DEPMDynamicAttributesAddin', 'DEPMDateTimeAddIn',
-                           'DEPMGenericAllocationAddin',
-                           'WDDataUploadAddIn', 'DEPMWorkflowAddin', 'DEPMWorkforceDesignAddIn']:
+        if procDir != 'DEPMWorkforceAddIn':
             for f in fls:
-                if f != file and f not in callstack:
+                if f != file and f not in callsTack:
                     fExt = pathlib.Path(f).suffix
                     if fExt == '.bs':
                         fPath = os.path.join(r, f)
@@ -59,33 +191,22 @@ def lookforusages(file, rootpath):
                             for ln in [li.strip('\n\r\t') for li in content]:
                                 if re.search(file.split('.')[0], ln, re.IGNORECASE) and re.search('#include', ln,
                                                                                                   re.IGNORECASE):
-                                    callstack.append(f)
-    return callstack
+                                    callsTack.append(f)
+                                    lookforusages(rpath, f, callsTack)
 
 
-def iterAllFiles(rootPath, checkFile, done, res):
-    done.append(checkFile)
-    callstack = lookforusages(checkFile, rootPath)
-    res[checkFile] = callstack
-    for el in callstack:
-        if el not in done:
-            iterAllFiles(rootPath, el, done, res)
-
-
-def mainsearch(rootPath, checkFile):
-    done = []
-    result = {}
-    iterAllFiles(rootPath, checkFile, done, result)
-    for key, value in result.items():
-        processSearchPrint(value)
-    print(json.dumps(result, indent=4))
+def checkProcess(process):
+    with open('data.txt') as reportsDict:
+        data = json.load(reportsDict)
+        for report, callStacks in data.items():
+            for calls in callStacks:
+                if process in calls:
+                    for call in calls:
+                        print(report + ";" + call)
 
 
 if __name__ == '__main__':
-    # checkRemaining(
-    #     'C:\\Farms\\DEPM-35267-WFB-Perf-Model-2403\\result.json')
-    # iterateDirectories('C:\\Farms\\DEPM-35267-WFB-Perf-Model-1204\\Projects\\AppEngine\\Sources\\Depm\\Processes\\', ['WB_CalculateAllActionsForEmployee.bs','WB_CalculateAllAssignmentsForEmployee.bs','WB_CalculateAllForOrg_Internal.bs','WB_CalculateFixedAmountBenefit.bs','WB_CalculatePremiumBenefit.bs','WB_CalculateSalaryAndDependenciesForEmployee.bs','WB_DeleteFixedAmountAction.bs','WB_DeletePremiumAction.bs','WB_PctBCActionCalculateAllAssigned.bs','WB_BCDefinitionDelete_Internal.bs','WB_PctBCActionDelete2.bs','WB_DeletePositionAssignment.bs','WB_ModifyAction.bs'])
-    # fromReportsToProcess('C:\\Farms\\2021-03-30\\2021-03-30\\processListSimple.txt', 'WB_PctBCCalculatePercentBCs.bs')
-    mainsearch(
-        'C:\\Farms\\DEPM-35267-WFB-Perf-Model-1204\\Projects\\AppEngine\\Sources\\Depm\\Processes\\',
-        'WB_AttributeChanged.bs')
+    # reportCallStacks()
+    # checkProcess('WB_ComputeAnnualFTE.bs')
+    # reportCallStacksClasses()
+    processAll('WB_ComputeAnnualFTE.bs')
