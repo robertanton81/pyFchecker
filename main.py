e4b8ee8dc9565aa15644c35a6135cbcae3651874
Robert Anton <robert.anton@infor.com>
2021-05-04 10:43:16 +0200

    final reverse tree pprint

--- main.py
+++ main.py
@@ -26,43 +26,10 @@ class ProcessNode:
         self.calling.append(calling)
 
 
-globalReportsNodes = []
-globalProcessNodes = []
-globalProcessNodesStr = []
-
-
-def processAll(file):
-    setNodes()
-    setReports()
-    for r in globalReportsNodes:
-        for n in r.nodes:
-            stack = []
-            populateStack(n, r, stack)
-            if file in stack:
-                print(f'\n******************* \n')
-                pprint_tree(n, r, file)
-
-
-def processAllReverse(file):
-    setNodes()
-    setReports()
-    for n in globalProcessNodes:
-        if n.key == file:
-            pprint_reverseTree(n)
-
-
-def populateStack(node, r, stack, file=None, _prefix="", _last=True):
-    child_count = len(node.calling)
-    for i, child in enumerate(node.calling):
-        _last = i == (child_count - 1)
-        stack.insert(0, child.key)
-        populateStack(child, r, stack, file, _prefix, _last)
-
-
 def pprint_reverseTree(node, file=None, _prefix="", _last=True):
-    print(_prefix, "`- " if _last else "|- ", node.key, sep="", file=file)
+    printKey = "\u0332".join(node.key) if isinstance(node, Report) else node.key
+    print(_prefix, "`- " if _last else "|- ", printKey, sep="", file=file)
     _prefix += "   " if _last else "|  "
-    child_count = 0
     if not isinstance(node, Report):
         child_count = len(node.callers)
         for i, child in enumerate(node.callers):
@@ -73,7 +40,8 @@ def pprint_reverseTree(node, file=None, _prefix="", _last=True):
 
 
 def pprint_tree(node, r, f, file=None, _prefix="", _last=True):
-    print(r.key, _prefix, "`- " if _last else "|- ", "\u0332".join(node.key) if f == node.key else node.key, sep="", file=file)
+    print(r.key, _prefix, "`- " if _last else "|- ", "\u0332".join(node.key) if f == node.key else node.key, sep="",
+          file=file)
     _prefix += "   " if _last else "|  "
     child_count = len(node.calling)
     for i, child in enumerate(node.calling):
@@ -81,17 +49,33 @@ def pprint_tree(node, r, f, file=None, _prefix="", _last=True):
         pprint_tree(child, r, f, file, _prefix, _last)
 
 
-def setReports():
-    globalProcessNodes.sort(key=lambda n: n.key, reverse=False)
+def apply(file):
+    reportsNodes = []
+    processNodes = []
+    processNodesStr = []
+    createNodes(processNodes, processNodesStr)
+    createReportNodes(processNodes, reportsNodes)
+    for node in processNodes:
+        for subNode in processNodes:
+            for call in subNode.calling:
+                if node.key.lower() == call.key.lower():
+                    node.callers.append(subNode)
+    for n in processNodes:
+        if n.key.lower() == file.lower():
+            pprint_reverseTree(n)
+
+
+def createReportNodes(processNodes, reportNodes):
+    processNodes.sort(key=lambda n: n.key, reverse=False)
     for reportKey, calledProcesses in readReportRef().items():
         curr = Report(reportKey)
-        globalReportsNodes.append(curr)
+        reportNodes.append(curr)
         processed = []
         for process in calledProcesses:
             if process not in processed:
                 processed.append(process)
                 procNode = None
-                for n in globalProcessNodes:
+                for n in processNodes:
                     if n.key.lower() == process.lower():
                         procNode = n
                         continue
@@ -100,18 +84,18 @@ def setReports():
                     procNode.setCaller(curr)
 
 
-def setNodes():
+def createNodes(processNodes, processNodesStr):
     for r, d, fls in os.walk(
             'C:\\Farms\\DEPM-35267-WFB-Perf-Model-1204\\Projects\\AppEngine\\Sources\\Depm\\Processes\\'):
         for f in fls:
             fExt = pathlib.Path(f).suffix
-            if fExt == '.bs' and f not in globalProcessNodesStr:
-                fPath = os.path.join(r, f)
+            if fExt == '.bs' and f not in processNodesStr:
                 node = ProcessNode(f)
                 if node is None:
                     print('is None!')
-                globalProcessNodes.append(node)
-                globalProcessNodesStr.append(f)
+                processNodes.append(node)
+                processNodesStr.append(f)
+                fPath = os.path.join(r, f)
                 with open(fPath, 'r', encoding='UTF-8') as fr:
                     content = fr.readlines()
                     for ln in [li.strip('\n\r\t') for li in content]:
@@ -119,14 +103,13 @@ def setNodes():
                             txt = re.split('\"', ln)
                             subNodeName = txt[1] + '.bs'
                             subNode = None
-                            for n in globalProcessNodes:
+                            for n in processNodes:
                                 if n.key.lower() == subNodeName.lower():
                                     subNode = n
-                                    break
-                            else:
+                                    continue
+                            if subNode is None:
                                 subNode = ProcessNode(subNodeName)
                             if subNode is not None:
-                                subNode.callers.append(node)
                                 node.calling.append(subNode)
 
 
@@ -147,5 +130,4 @@ def readReportRef():
 
 
 if __name__ == '__main__':
-    # processAll('WB_ComputeAnnualFTE.bs')
-    processAllReverse('WB_ComputeAnnualFTE.bs')
+    apply('WB_ComputeAnnualFTE.bs')
