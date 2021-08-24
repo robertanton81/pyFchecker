a38c4ad391138a47204990e4adde3d06171e0e1c
robertanton81 <18146018+robertanton81@users.noreply.github.com>
2021-08-24 16:44:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -26,34 +26,33 @@ class ProcessNode:
         self.calling.append(calling)
 
 
-def pprint_reverseTree(node, file=None, _prefix="", _last=True):
-    printKey = "\u0332".join(node.key) if isinstance(node, Report) else node.key
+def pprint_reverseTree(node, level, file=None, _prefix="", _last=True, _counter=-1):
+    printKey = "\u0332".join(node.key) if isinstance(
+        node, Report) else node.key
     print(_prefix, "`- " if _last else "|- ", printKey, sep="", file=file)
     _prefix += "   " if _last else "|  "
     if not isinstance(node, Report):
+        _counter += 1
         child_count = len(node.callers)
         for i, child in enumerate(node.callers):
+            if _counter == level:
+                break
             _last = i == (child_count - 1)
-            pprint_reverseTree(child, file, _prefix, _last)
+            pprint_reverseTree(child,
+                               level,
+                               file,
+                               _prefix,
+                               _last,
+                               _counter,)
     else:
         pass
 
 
-def pprint_tree(node, r, f, file=None, _prefix="", _last=True):
-    print(r.key, _prefix, "`- " if _last else "|- ", "\u0332".join(node.key) if f == node.key else node.key, sep="",
-          file=file)
-    _prefix += "   " if _last else "|  "
-    child_count = len(node.calling)
-    for i, child in enumerate(node.calling):
-        _last = i == (child_count - 1)
-        pprint_tree(child, r, f, file, _prefix, _last)
-
-
-def apply(file):
+def apply(file, path, level=99999999):
     reportsNodes = []
     processNodes = []
     processNodesStr = []
-    createNodes(processNodes, processNodesStr)
+    createNodes(processNodes, processNodesStr, path)
     createReportNodes(processNodes, reportsNodes)
     for node in processNodes:
         for subNode in processNodes:
@@ -62,7 +61,23 @@ def apply(file):
                     node.callers.append(subNode)
     for n in processNodes:
         if n.key.lower() == file.lower():
-            pprint_reverseTree(n)
+            pprint_reverseTree(n, level)
+
+
+def get_obsolete(path):
+    reportsNodes = []
+    processNodes = []
+    processNodesStr = []
+    createNodes(processNodes, processNodesStr, path)
+    createReportNodes(processNodes, reportsNodes)
+    for node in processNodes:
+        for subNode in processNodes:
+            for call in subNode.calling:
+                if node.key.lower() == call.key.lower():
+                    node.callers.append(subNode)
+    for n in processNodes:
+        if len(n.callers) == 0:
+            pprint_reverseTree(n, 999999999999)
 
 
 def createReportNodes(processNodes, reportNodes):
@@ -84,24 +99,23 @@ def createReportNodes(processNodes, reportNodes):
                     procNode.setCaller(curr)
 
 
-def createNodes(processNodes, processNodesStr):
-    for r, d, fls in os.walk(
-            'C:\\Farms\\DEPM-35267-WFB-Perf-Model-1204\\Projects\\AppEngine\\Sources\\Depm\\Processes\\'):
+def createNodes(processNodes, processNodesStr, path):
+    for r, d, fls in os.walk(path):
         for f in fls:
             fExt = pathlib.Path(f).suffix
-            if fExt == '.bs' and f not in processNodesStr:
+            if fExt == ".bs" and f not in processNodesStr:
                 node = ProcessNode(f)
                 if node is None:
-                    print('is None!')
+                    print("is None!")
                 processNodes.append(node)
                 processNodesStr.append(f)
                 fPath = os.path.join(r, f)
-                with open(fPath, 'r', encoding='UTF-8') as fr:
+                with open(fPath, "r", encoding="UTF-8") as fr:
                     content = fr.readlines()
-                    for ln in [li.strip('\n\r\t') for li in content]:
-                        if re.search('#include', ln, re.IGNORECASE):
-                            txt = re.split('\"', ln)
-                            subNodeName = txt[1] + '.bs'
+                    for ln in [li.strip("\n\r\t") for li in content]:
+                        if re.search("#include", ln, re.IGNORECASE):
+                            txt = re.split('"', ln)
+                            subNodeName = txt[1] + ".bs"
                             subNode = None
                             for n in processNodes:
                                 if n.key.lower() == subNodeName.lower():
@@ -114,20 +128,28 @@ def createNodes(processNodes, processNodesStr):
 
 
 def readReportRef():
-    rpath = '.\\assets\\processListSimple.txt'
-    with open(rpath, 'r') as fr:
+    rpath = ".\\sources\\processListSimple.txt"
+    with open(rpath, "r") as fr:
         content = fr.readlines()
-        curr = ''
+        curr = ""
         repsDict = {}
-        for ln in [li.strip('\n\r') for li in content]:
-            if not ln.startswith('\t'):
-                if ln not in repsDict and ln != '':
+        for ln in [li.strip("\n\r") for li in content]:
+            if not ln.startswith("\t"):
+                if ln not in repsDict and ln != "":
                     repsDict[ln] = []
                     curr = ln
-            elif ln.startswith('\t') and curr != '':
-                repsDict[curr].append(ln.strip('\t')[1:-1] + '.bs')
+            elif ln.startswith("\t") and curr != "":
+                repsDict[curr].append(ln.strip("\t")[1:-1] + ".bs")
         return repsDict
 
 
-if __name__ == '__main__':
-    apply('WB_ComputeAnnualFTE.bs')
+if __name__ == "__main__":
+    # TODO: needs to match correct path to bs files
+    path = "C:\\Farms\\WFB_0207_model\\Projects\\AppEngine\\Sources\\Depm\\Processes\\"
+
+    # this finds all calls to the bs process (.bs file)
+    # TODO: to limit number of levels, add third argument (int)
+    apply("WB_AttributeChangedAssignment_Internal.bs", path)
+
+    # TODO: this should print all unreferenced bs files
+    # get_obsolete(path)
